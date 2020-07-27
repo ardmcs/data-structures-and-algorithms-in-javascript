d4f4858c53ef11e915d64b360eb1b186c16428a3
Zachary Zhou <avengyy@gmail.com>
2020-07-27 12:18:51 +0800

    📦 Add helper comments to insertion sort

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -1,11 +1,16 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
+    // Start with second element in array
     let currentVal = arr[i];
+    // Set pointer to element before
     let j = i - 1;
+    // Compare if element before is greater than current
     while (j >= 0 && arr[j] > currentVal) {
+      // Iteratively bubble all the greater elements to the right
       arr[j + 1] = arr[j];
       j--;
     }
+    // Assign the smallest value to the left (Since j will be i - 1)
     arr[j + 1] = currentVal;
   }
   return arr;
