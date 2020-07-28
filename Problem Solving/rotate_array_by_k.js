eacb8d3add0731f10045c1e955aad7ba4cab0fff
Zachary Zhou <avengyy@gmail.com>
2020-07-28 23:18:48 +0800

    📦 Add rotate array by k (Leetcode)

---
+++ Problem Solving/rotate_array_by_k.js
@@ -0,0 +1,24 @@
+/**
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums, k) {
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+}
+
+function reverse(arr, start, end) {
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+rotate([1, 2, 3, 4, 5, 6, 7], 3); // [5,6,7,1,2,3,4]
