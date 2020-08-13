76f36b13f1f2971fcdcb32cff389fd9adc2dd046
Zachary Zhou <avengyy@gmail.com>
2020-08-13 12:16:44 +0800

    🧹 Simplify first unique char

--- Problem Solving/first_uniq_char.js
+++ Problem Solving/first_uniq_char.js
@@ -6,34 +6,26 @@
  * @return {number}
  */
 function firstUniqChar(s) {
-  // Create a hash map
-  let map = new Map();
-
-  for (let i = 0; i < s.length; i++) {
-    let current = s[i];
-    // Set unique values' index into hash map
-    if (!map.has(current)) {
-      map.set(current, i);
-    } else {
-      // If not unique, set index to -1
-      map.set(current, -1);
+  let map = {};
+    
+    for (let i=0; i<s.length; i++) {
+        let char = s[i];
+        if (map[char]) {
+            map[char] += 1;
+        } else {
+            map[char] = 1;
+        }
     }
-  }
-
-  // Define minimum index position
-  let min = Infinity;
-
-  // Iterate through the keys of the hash map (which are the characters)
-  for (let c of map.keys()) {
-    // If the char is unique and is lesser than the new minimum index
-    if (map.get(c) > -1 && map.get(c) < min) {
-      // Assign minimum index to the 'found' char
-      min = map.get(c);
+    
+    for (let i=0; i<s.length; i++) {
+        let char = s[i];
+        
+        if (map[char] === 1) {
+            return i;
+        }
     }
-  }
-
-  // If min stays put, return -1 (No unique char)
-  return min === Infinity ? -1 : min;
+    
+    return -1;
 }
 
 firstUniqChar("loveleetcode"); // 2
