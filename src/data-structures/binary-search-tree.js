c94b9fe8e5c08974493e17e59140e364d7809674
Zac Zhou <avengyy@gmail.com>
2020-09-16 14:08:28 +0800

    📦 Add DFS to BST

--- src/data-structures/binary-search-tree.js
+++ src/data-structures/binary-search-tree.js
@@ -87,16 +87,54 @@ class BinarySearchTree {
 
     return data;
   }
+  DFSPreOrder() {
+    let data = [];
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
+  DFSPostOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
+  DFSInOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
 }
 
 var tree = new BinarySearchTree();
 
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
-console.log(tree.BFS());
+console.log(tree.DFSInOrder());
